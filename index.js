f35791f2f8143d135c611df8511d9827260fe347
Ubuntu <ubuntu@ip-172-31-8-39.us-west-2.compute.internal>
2016-10-04 06:12:30 +0000

    Changed to open() and read()

--- index.js
+++ index.js
@@ -2,10 +2,37 @@ var fs = require('fs');
 var http = require('http');
 var path = require('path');
 var filePath = path.join(__dirname, '/public', 'index.html');
-var data = fs.readFileSync(filePath, 'utf8');
 
-http.createServer(function(request, response) {
-	response.writeHead(200, {'Content-Type': 'text/html'});
-	response.write(data);
-	response.end();
-}).listen(8080);
+fs.stat(filePath, (err, stats) =>
+	{
+		if(err)
+		{
+			console.log(err);
+		}
+		else
+		{
+			console.log(stats.size);
+			var buff = Buffer.alloc(stats.size, 1);
+			fs.open(filePath, 'r', function (err, fd) {
+				if(err)
+				{
+					return console.log(err);
+				}
+				else
+				{
+					fs.read(fd, buff, 0, stats.size)
+					var data = buff.toString('utf8');
+					http.createServer(function(request, response) 
+					{
+						response.writeHead(200, {'Content-Type': 'text/html'});
+						response.write(data);
+						response.end();
+					}).listen(8080);
+				}
+			});
+		}
+	}); //messy callbacks, fix later
+
+
+
+
