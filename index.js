d1f40f624ea7525e9cb48c9f3be6b42b42ebf2f3
Ubuntu <ubuntu@ip-172-31-8-39.us-west-2.compute.internal>
2016-10-04 03:28:02 +0000

    make it better(readFileSync)

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
 var fs = require('fs');
 var http = require('http');
 var path = require('path');
-var filePath = path.join(__dirname, '/public', 'index.html')
+var filePath = path.join(__dirname, '/public', 'index.html');
+var data = fs.readFileSync(filePath, 'utf8');
 
 http.createServer(function(request, response) {
-
-	fs.readFile(filePath, 'utf8', function (err,data) {
-		if (err) 
-		{
-			return console.log(err);
-		}
-		response.writeHead(200, {'Content-Type': 'text/html'});
-		response.write(data);
-		response.end();
-	});
-	
+	response.writeHead(200, {'Content-Type': 'text/html'});
+	response.write(data);
+	response.end();
 }).listen(8080);
-
